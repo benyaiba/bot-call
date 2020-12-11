1618098975a1d4e0f012a619543255514f50ea6b
yaiba <yaibamx@gmail.com>
2020-12-11 16:24:41 +0800

    Update app.js

--- app.js
+++ app.js
@@ -178,7 +178,7 @@ function handleMessage(sender_psid, workplace_response) {
 				  }
 				}
 		let buttons = [];
-		for(var i=0;i<1;i++) {
+		for(var i=0;i<3;i++) {
 			console.log(array[i]);
 			let temp = {
 				"type":"postback",
